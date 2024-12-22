51666ccdb3580f9a4deff598779c0a1e7eb033e6
MASHANTHINI <156877917+MASHANTHINI@users.noreply.github.com>
2024-12-22 11:18:35 -0500

    Create solution.c

---
+++ solution.c
@@ -0,0 +1,20 @@
+int lengthOfLongestSubstring(char* s) {
+   int max=0;
+   int len=0,a=0;
+   int count[256]={0};
+   for(int i=0;i<strlen(s);i++){
+    count[(unsigned char)s[i]]++;
+    len++;
+    
+    while(count[(unsigned char)s[i]]>1){
+        count[(unsigned char)s[a]]--;
+        a++;
+        len--;
+    }
+    if(len>max)
+    max=len;
+    
+   }
+   return max;
+    
+}
